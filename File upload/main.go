e62fb37e688bd4b104ecd71c8605afe154afffd3
Thomas Boccinfuso <tboccinfuso@gmail.com>
2018-08-11 16:29:24 -0400

    documentation

--- File upload/main.go
+++ File upload/main.go
@@ -2,33 +2,34 @@ package main
 
 import "net/http"
 import "log"
-import "io/ioutil"
+import "io/ioutil" // package to handle read / write
 import "fmt"
 
 func main() {
 	http.HandleFunc("/", fileUpload)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", nil) // create server on localhost:8000
 }
 
 func fileUpload(w http.ResponseWriter, req *http.Request) {
 	var s string
 
-	if req.Method == http.MethodPost {
-		f, _, err := req.FormFile("file")
-		if err != nil {
+	if req.Method == http.MethodPost { // if method is POST
+		f, _, err := req.FormFile("file") // request the file (upload)
+		if err != nil { // print error 
 			log.Println(err)
 			http.Error(w, "Error uploading the file", http.StatusInternalServerError)
 			return
 		}
 		defer f.Close()
-		bs, err := ioutil.ReadAll(f)
+		bs, err := ioutil.ReadAll(f) // read the file
 		if err != nil {
 			log.Println(err)
 			http.Error(w, "Error reading the file", http.StatusInternalServerError)
 			return
 		}
-		s = string(bs)
+		s = string(bs) // print file name once it has been uploaded
 	}
+	// display the following html when on homepage
 	w.Header().Set("CONTENT-TYPE", "text/html; enc=UTF-8")
 	fmt.Fprintf(w, `<form action "/" method="post" enctype="multipart/form-data">
                    Choose file to upload:<br>
