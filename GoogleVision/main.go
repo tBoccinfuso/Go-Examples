94f35685cab784f6d9c6028c8e7c0c19cf8259b2
Thomas Boccinfuso <tboccinfuso@gmail.com>
2018-08-11 16:33:47 -0400

    updated documentation

--- GoogleVision/main.go
+++ GoogleVision/main.go
@@ -23,23 +23,23 @@ func main() {
 	// Sets the name of the image file to annotate.
 	filename := "Picture1.png"
 
-	file, err := os.Open(filename)
+	file, err := os.Open(filename) // open file picker
 	if err != nil {
 		log.Fatalf("Failed to read file: %v", err)
 	}
 	defer file.Close()
-	image, err := vision.NewImageFromReader(file)
+	image, err := vision.NewImageFromReader(file) // set a new instance of a Google vision image with our uploaded image
 	if err != nil {
 		log.Fatalf("Failed to create image: %v", err)
 	}
 
-	labels, err := client.DetectLabels(ctx, image, nil, 10)
+	labels, err := client.DetectLabels(ctx, image, nil, 10) // store the returned labels from Google Vision
 	if err != nil {
 		log.Fatalf("Failed to detect labels: %v", err)
 	}
 
 	fmt.Println("Labels:")
 	for _, label := range labels {
-		fmt.Println(label.Description)
+		fmt.Println(label.Description) // Loop through all the labels and print them out in the console
 	}
 }
